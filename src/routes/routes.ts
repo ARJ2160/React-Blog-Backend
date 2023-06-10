6558c1bbb29eb46afe791ce67414726732b7a1e5
Atharva <atharvaj2160@gmail.com>
2023-06-10 10:10:31 +0530

    migrate to rewrites

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -40,7 +40,7 @@ router.get("/postsdata/:_id", (req: Request, res: Response) => {
 //<---------------------------- Post On the Posts Database ---------------------------->
 
 router.post("/postsdata", (req: Request, res: Response) => {
-  console.log(">>",req.body)
+  console.log(">>", req.body);
   const db = req.body;
   Posts.create(db, (err) => {
     if (err) {
